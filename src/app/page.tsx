cab1bd9e0612e102d5dbb92733214bbc91ca75e8
Devtimmy <oyinloyematthew6@gmail.com>
2025-08-01 11:21:24 +0100

    Feat: About to start working on the productivity page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import Navbar from "@/components/Navbar";
 
 export default function HomePage() {
   return (
-
-<>
-        <Navbar/>
-
+    <>
+      <Navbar/>
       <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold">This is Refhra&apos;s web Home page</h1>
       </div>
-</>
+    </>
 
   )
 }
